d24e729cb677f813cdc8f2ac156b8ab48e8e663c
Yuvraj <yuviw2711@gmail.com>
2024-07-18 18:33:09 +0530

    Added Charts

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkoutService } from '../workout.service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-chart',
-  standalone: true,
-  imports: [],
+  selector: 'app-workout-display',
+  standalone:true,
+  imports:[NgxChartsModule,CommonModule],
   templateUrl: './chart.component.html',
-  styleUrl: './chart.component.css'
+  styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+  users: any[] = [];
+  selectedUser: any = null;
+  chartData: any[] = [];
+  view: [number, number] = [700, 400];
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
 
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit(): void {
+    this.users = this.workoutService.getWorkouts();
+  }
+
+  selectUser(user: any): void {
+    this.selectedUser = user;
+    this.prepareChartData();
+  }
+
+  prepareChartData(): void {
+    this.chartData = this.selectedUser.workouts.map((workout: any) => ({
+      name: workout.type,
+      value: workout.minutes
+    }));
+  }
 }
