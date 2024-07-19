c6a7c1165e7037ca885b643d055d23680b05050d
Yuvraj <yuviw2711@gmail.com>
2024-07-19 13:55:15 +0530

    Final

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.{html,ts}',  // Include all HTML and TypeScript files in the src directory
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
